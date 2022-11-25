c44e8461ad54729bd91bd10dcdb8f0fa35816d7e
devilasu <73906066+devilasu@users.noreply.github.com>
2022-11-25 14:16:58 +0900

    first commit

---
+++ src/main/java/com/ewp/EwpApplication.java
@@ -0,0 +1,13 @@
+package com.ewp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwpApplication.class, args);
+	}
+
+}
